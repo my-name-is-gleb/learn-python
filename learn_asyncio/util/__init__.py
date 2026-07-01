1c8380726f6a04d2b904c57728681040689a1a97
my-name-is-gleb <startingithub2025@gmail.com>
2026-07-01 19:20:06 +0300

    clean: remove deleted root files from repository

--- learn_asyncio/util/__init__.py
+++ learn_asyncio/util/__init__.py
@@ -1,3 +1,4 @@
 from util.delay_functions import delay
 from util.async_timer import async_timed
 from util.chapter_04 import fetch_status
+from util.my_password import password
--- test_eval.py
+++
@@ -1,8 +0,0 @@
-user_cod = input("> ")
-
-a = eval(user_cod)
-
-if a == True:
-    print("good")
-else:
-    print("bad")
